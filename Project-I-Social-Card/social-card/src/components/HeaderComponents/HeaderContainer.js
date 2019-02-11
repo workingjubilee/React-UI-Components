03040a58ff5222ba628dc16f4107528620690bd0
workingjubilee <workingjubilee@gmail.com>
2019-02-11 14:02:01 -0800

    Completed unstyled Header with import/export routing.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+
+function HeaderContainer() {
+  return (
+    <div className="header">
+      <ImageThumbnail />
+      <div className="headerText">
+        <HeaderTitle />
+        <HeaderContent />
+      </div>
+    </div>
+  );
+}
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -4,6 +4,3 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!
-// Get started with React
-// React makes it painless to create interactive UIs. Design simple views for each state in your application
